802a613bb61cbcaf191893d06f471bf4809e57d2
Arkapravo Rajkonwar <arkapravocollegemail@gmail.com>
2026-04-01 12:00:00 +0530

    debugging

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use((err, req, res, next) => {
 // ---------------------
 // Start Server
 // ---------------------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
     console.log(`CareTree API running on port ${PORT}`);
 });
